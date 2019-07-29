6dc03854071e7bf57d5854db2a48497425c694b5
Saiful1401054 <53428304+Saiful1401054@users.noreply.github.com>
2019-07-29 15:51:34 +0600

    Update insertion_sort.c

--- insertion_sort.c
+++ insertion_sort.c
@@ -1,46 +1,28 @@
 #include <stdio.h>
-
-int main()
-{
-  
+int main(){
   int a[20],i,j,n,temp;
- 
-  printf("Enter the total number \n");
   
+  printf("Enter the total number \n");
   scanf("%d",&n);
    
   printf("Enter the array element \n");
- 
-   for(i=0;i<n;i++)
-    scanf("%d",&a[i]);
+  for(i=0;i<n;i++)
+  scanf("%d",&a[i]);
 
     for(i=1;i<n;i++){
- 
        for(j=i;j>0;j--){
-     
-         if(a[j-1]>a[j]){
-       
-           temp=a[j];
-           
-           a[j]=a[j-1];
-          
-           a[j-1]=temp;
-            
-          }
-       
+          if(a[j-1]>a[j]){
+            temp=a[j];
+            a[j]=a[j-1];
+            a[j-1]=temp;
+            }
        }
+    }
    
-     }
-   
- for(j=0;j<n;j++)
-   
+ for(j=0;j<n;j++)  
  {
         printf("%d ",a[j]);
-  
-  }
-
-    
+  }   
 return 0;
-
 }
 
